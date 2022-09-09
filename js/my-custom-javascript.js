e0fa8b71cb96914eaee3a865ee8529d3762383db
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-09-09 09:51:55 -0500

    id selectors done

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,9 +1,22 @@
 "use strict";
-$(function() {
-    alert( 'The DOM has finished loading!');
-});
+//Introduction Exercise
+// $(function() {
+//     alert( 'The DOM has finished loading!');
+// });
+
+//Selectors Exercise
+
+//#3
 // let contents = $('#lorem').html();
 // alert(contents);
 
-let contentsFour = $('#goodlist').html();
-alert(contentsFour);
+//#4
+// let contentsFour = $('#goodlist').html();
+// alert(contentsFour);
+
+//#5
+// let contentsFive = $('#goodlist').html();
+// alert(contentsFive);
+//ow does it change the selection, it does not work, each id should be unique
+
+//Class Selectors
