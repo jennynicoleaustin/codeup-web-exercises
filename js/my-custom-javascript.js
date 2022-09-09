90c4f0c608067ae19a0119806c3647f5ef771c60
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-09-09 11:40:01 -0500

    mouse events #2

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -31,11 +31,23 @@ $(function() {
 //Element Selectors
 
 //#2
-$('li').css('font-size', '20px');
+// $('li').css('font-size', '20px');
 
 //#3
-$('h1, p, li').css('background-color', 'yellow');
+// $('h1, p, li').css('background-color', 'yellow');
 
 //#4
-let contentsH = $('h1').html();
-    alert(contentsH);
+// let contentsH = $('h1').html();
+//     alert(contentsH);
+
+//JQuery Events
+
+//Mouse Events
+
+//#2
+// let clickColor = function (e) {
+//     $('h1').css('background-color', 'pink');
+// };
+$('h1').click(function() {
+    $('h1').css('background-color', 'pink');
+});
