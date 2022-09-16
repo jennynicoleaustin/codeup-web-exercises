109c7005ac408daddfbfe7b4b52df340325c35f2
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-09-16 14:19:36 -0500

    on to functions

--- dino_practice/udemy_practice/Loops_CODE/IteratingObjects/app.js
+++ dino_practice/udemy_practice/Loops_CODE/IteratingObjects/app.js
@@ -1,3 +1,5 @@
+//:::: ITERATING OVER OBJECTS:::::
+
 const testScores = {
     keenan: 80,
     damon: 67,
@@ -10,23 +12,52 @@ const testScores = {
     diedre: 81,
     vonnie: 60
 }
+// for of cannot be used to iterate over an object literal since it is not technically an object. instead we use the FOR IN loop
+
 // ===================================
 // USING FOR...IN TO ITERATE AN OBJECT
 // ===================================
 
-for (let person in testScores) {
-    console.log(`${person} scored ${testScores[person]}`);
-}
+//for in only gives up the key of an object.
+// for (let key in testScores) {
+//     console.log(key)
+// } // this gives only the key, not the key and value...
+//
+// // One option to key both the key and the value to print out is...
+// for (let person in testScores) {
+//     console.log(`${person} scored ${testScores[person]}`);
+// }
 
 // ==============================================
 // Using Object methods to iterate
 // (turn data into an array and then use for...of)
 // ==============================================
 
-let total = 0;
-let scores = Object.values(testScores);
+// Object.values turns the values of the associated keys into an array
+// Object.keys turns the keys into an array
+//Object.entries gives in array, each item in the array is [key, value]
+
+// sum up all the scores and average them. save the average to a variable
+
+// need to get all the values
+// add all values together
+// divide sum by number of values
+
+let total = 0 // make a variable for your for of to add to
+let scores = Object.values(testScores)
 for (let score of scores) {
     total += score;
 }
-console.log(total / scores.length)
+let average = total/scores.length
+
+
+
+
+
+// let total = 0;
+// let scores = Object.values(testScores);
+// for (let score of scores) {
+//     total += score;
+// }
+// console.log(total / scores.length)
 
