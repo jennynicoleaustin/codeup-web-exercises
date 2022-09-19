2d587a0c43e3f60256449a77ff5b960f7fdc29e9
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-09-19 13:33:21 -0500

    finally finished intro function exercises

--- dino_practice/udemy_practice/Functions_Intro_CODE/app.js
+++ dino_practice/udemy_practice/Functions_Intro_CODE/app.js
@@ -84,6 +84,123 @@ function add(x, y) {
     }
     return x + y;
 }
+// You can only return a single value. (value can be an array or oject)
+
+// It's time to practice returning values from a function! Write a simple function multiply which accepts two numerical arguments and returns their product (multiply them together).  Make sure to return the value instead of printing it!
+//
+// multiply(2,3) // 6
+// multiply(9,9) // 81
+// multiply(5,4) // 20
+
+function multiply (x, y) {
+    let sum = x * y;
+    return sum;
+}
+
+multiply(1,3)
+
+// I often struggle to know whether I should wear shorts or pants on a given day. (this is a complete lie.  It's really not that hard to decide.) Please help me decide by writing me a function called isShortsWeather.
+//
+// It should accept a single number argument, which we will call temperature(but you can name it whatever you want, of course).
+// If temperature is greater than or equal to 75, return true.
+// Otherwise, return false.
+// This exercise assumes temperature is in Fahrenheit.  I apologize to all my Celsius-using students!
+//
+// isShortsWeather(80) //true
+// isShortsWeather(48) //false
+// isShortsWeather(75) //true
+function isShortsWeather (temp) {
+    if (temp >= 75) {
+        return true;
+    }
+    return false
+}
+
+// Please write a function called lastElement which accepts a single array argument.  The function should return the last element of the array (without removing the element).  If the array is empty, the function should return null.
+//
+// lastElement([3,5,7]) //7
+// lastElement([1]) //1
+// lastElement([]) //null
+
+function lastElement (array) {
+    let lastEl = (array.length - 1)
+    if (lastEl === -1) {
+        return null
+    } else
+        return array[lastEl]
+}
+
+let myArray = ['jenny', 'billy', 'nikki', 'petey']
+
+let emptyArray = []
+
+// Define a function called capitalize that accepts a string argument and returns a new string with the first letter capitalized (but the rest of the string unchanged).  For example:
+//
+// capitalize('eggplant') // "Eggplant"
+// capitalize('pamplemousse') // "Pamplemousse"
+// capitalize('squid') //"Squid"
+
+// Hints:
+// Remember that strings are immutable, meaning that you cannot simply change the first letter in the original string.  You will need to make a new string that you return
+// Single out the first letter and capitalize it. (use a string method to help!)
+// Add that first letter to the rest of the original string, sliced to omit the original first letter (use a string method to help!)
+//
+// For example: 'eggplant' becomes 'E' + 'ggplant'
+
+function capitalize (string) {
+    let firstLetter = string[0].toUpperCase();
+    let lastIndex = (string.length + 1);
+    let restString = string.substring(1, lastIndex).toLowerCase();
+    let newString = `${firstLetter}${restString}`;
+    return newString;
+}
+
+// Write a function called sumArray which accepts a single argument: an array of numbers.  It should return the sum of all the numbers in the array.
+//
+// sumArray([1,2,3]) // 6
+// sumArray([2,2,2,2]) // 8
+// sumArray([50,50,1]) // 101
+// Hints:
+// You'll need a variable to keep track of the total.  It should start out as zero.
+// Loop over the array and for each element, add it to the total variable.
+// After you have added every number to total, return total.
+
+function sumArray(numArray) {
+    let total = 0
+    for (let i = 0; i <= (numArray.length -1); i++) {
+        total += numArray[i]
+        // console.log(numArray[i])
+    } return total
+}
+let testArray = [1, 2, 3, 4]
+
+// Write a function called returnDay. this function takes in one parameter (a number from 1-7) and returns the day of the week (1 is Monday, 2 is Tuesday, etc.)  If the number is less than 1 or greater than 7, the function should return null. In some countries Sunday is treated as the first day of the week, but for this exercise we will use Monday as the first day.
+//
+// Hint: store the days of the week in an array, or use an object with numerical keys.  When the function is called, plug the number into the array/object you've created to retrieve the corresponding day name and then return that value.
+// returnDay(1) // "Monday"
+// returnDay(7) // "Sunday"
+// returnDay(4) // "Thursday"
+// returnDay(0) // null
+//
+// const theWeek = [
+//     {name: 'Monday', number: 1},
+//     {name: 'Tuesday', number: 2},
+//     {name: 'Wednesday', number: 3},
+//     {name: 'Thursday', number: 4},
+//     {name: 'Thursday', number: 4},
+//     {name: 'Thursday', number: 4},
+//     {name: 'Sunday', number: 7}
+//     ]
+const theWeek =
+    {1:'Monday', 2:'Tuesday', 3:'Wednesday', 4:'Thursday', 5:'Friday', 6:'Saturday', 7:'Sunday'}
+function returnDay (num) {
+    if (num < 1 || num > 7) {
+        return null;
+    }
+    return (theWeek[num])
+}
+
+
 
 
 
