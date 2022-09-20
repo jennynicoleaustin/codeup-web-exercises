4a773d2aa6bc01c8effc1f701d81cb7f2ce51051
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-09-20 10:25:09 -0500

    es6, except the object destructuring

--- js/es6.js
+++ js/es6.js
@@ -1,3 +1,4 @@
+// GO BACK AND FINISH THE OBJECT DESTRUCTURING
 /*
  * Complete the TODO items below
  */
@@ -26,9 +27,11 @@ const users = [
     name: 'justin',
     email: 'justin@codeup.com',
     languages: ['html', 'css', 'javascript', 'php']
-  }
+  },
   {
-    name: 'jenny'
+    name: 'jenny',
+    email: 'jennynicoleaustin@gmail.com',
+    languages: ['html', 'css', 'javascript']
   }
 ];
 
@@ -36,31 +39,37 @@ const users = [
 // to the languages array
 // TODO: replace the `var` keyword with `const`, then try to reassign a variable
 // declared as `const`
-var name = 'your_name_here';
-var email = '';
-var languages = [];
+const name = 'your_name_here';
+const email = '';
+const languages = [];
 
 // TODO: rewrite the object literal using object property shorthand
-users.push({
-  name: name,
-  email: email,
-  languages: languages
-});
+// users.push({
+//   name: name,
+//   email: email,
+//   languages: languages
+// });
+// users.push({})
 
 // TODO: replace `var` with `let` in the following variable declarations
-var emails = [];
-var names = [];
+let emails = [];
+let names = [];
 
 // TODO: rewrite the following using arrow functions
-users.forEach(function(user) {
-  return emails.push(user.email);
-});
-users.forEach(function(user) {
-  return names.push(user.name);
-});
+// users.forEach(function(user) {
+//   return emails.push(user.email);
+// });
+
+users.forEach(user => (emails.push(user.email)));
+
+// users.forEach(function(user) {
+//   return names.push(user.name);
+// });
+
+users.forEach(user => (names.push(user.name)));
 
 // TODO: replace `var` with `let` in the following declaration
-var developers = [];
+let developers = [];
 users.forEach(function(user) {
   // TODO: rewrite the code below to use object destructuring assignment
   //       note that you can also use destructuring assignment in the function
@@ -70,16 +79,18 @@ users.forEach(function(user) {
   const languages = user.languages;
 
   // TODO: rewrite the assignment below to use template strings
-  developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));
+  developers.push(`${name}'s email is ${email} ${name} knows ${languages.join(', ')}`);
 });
 
 // TODO: Use `let` for the following variable
-var list = '<ul>';
+let list = '<ul>';
 
 // TODO: rewrite the following loop to use a for..of loop
-developers.forEach(function (developer) {
+// TODO: rewrite the assignment below to use template strings
 
-  // TODO: rewrite the assignment below to use template strings
+for (let developer of developers) {`${list} <li> ${developer} </li> ${list} </ul>`};
+
+developers.forEach(function (developer) {
   list += '<li>' + developer + '</li>';
 });
 list += '</ul>';
